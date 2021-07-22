39cb7e418905583ada774d06e333f9e567b4292a
Sardo Ip <sardo.ip@sardo.work>
2021-07-22 17:36:39 +0800

    Complete add contact function and comment

--- client.php
+++ client.php
@@ -8,14 +8,89 @@ class CivicrmClient {
     $this->api_key = $api_key;
     $this->site_key = $site_key;
   }
-  public function getContact($group = NULL){
+
+/**
+ * Get all contact in the CRM or in a group.
+ *
+ * @param string $group The group of the contacts. Can be optional.
+ * @return stdClass $result The result which returned by the API.
+ */
+
+  public function getContacts($group = NULL){
+    $group_json = json_encode(['group' => $group]);
     $params = [
       'key' => $this->site_key,
       'api_key' => $this->api_key,
       'entity' => 'contact',
       'action' => 'get',
-      'json' => 1,
+      'json' => $group_json,
     ];
+
+    if(!$group){
+      $params['json'] = 1;
+    }
+
+    $params_string = http_build_query($params);
+    $api_url = "{$this->base_url}?{$params_string}";
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $api_url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $json_result = curl_exec($curl);
+    curl_close($curl);
+
+    $result = json_decode($json_result);
+
+    return $result;
+  }
+
+  /**
+   * Search contact in the CRM by given information.
+   *
+   * @param array $contact_info An array which contains all required contact information.
+   * @return stdClass $result The result which returned by the API.
+   */
+
+  public function searchContact($contact_info){
+    $contact_info_json = json_encode($contact_info);
+    $params = [
+      'key' => $this->site_key,
+      'api_key' => $this->api_key,
+      'entity' => 'contact',
+      'action' => 'get',
+      'json' => $contact_info_json,
+    ];
+    $params_string = http_build_query($params);
+    $api_url = "{$this->base_url}?{$params_string}";
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $api_url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $json_result = curl_exec($curl);
+    curl_close($curl);
+
+    $result = json_decode($json_result);
+
+    return $result;
+  }
+
+  /**
+   * Add contact in the CRM by given information.
+   *
+   * @param array $contact_info An array which contains all required contact information.
+   * @return stdClass $result The result which returned by the API.
+   */
+
+  public function addContact($contact_info){
+    $contact_info_json = json_encode($contact_info);
+    $params = [
+      'key' => $this->site_key,
+      'api_key' => $this->api_key,
+      'entity' => 'contact',
+      'action' => 'create',
+      'json' => $contact_info_json,
+    ];
+
     $params_string = http_build_query($params);
     $api_url = "{$this->base_url}?{$params_string}";
 
